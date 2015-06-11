3f0158124238013768116fc9982e763f16613a31
maria <mariiromero17@gmail.com>
2015-06-11 18:08:12 -0430

    Crud proveedores

---
+++ db/migrate/20150611222220_create_suppliers.rb
@@ -0,0 +1,15 @@
+class CreateSuppliers < ActiveRecord::Migration
+  def change
+    create_table :suppliers do |t|
+      t.string :name, null: false, limit: 70
+      t.string :alias, null: false, limit: 20
+      t.string :rif, null: false, limit: 12
+      t.integer :fiscal_address, null: false
+      t.integer :comercial_address, null: false
+      t.string :phone, null: false, limit: 15
+      t.string :email, null: false, limit: 40
+
+      t.timestamps null: false
+    end
+  end
+end
